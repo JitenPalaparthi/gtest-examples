4962b68b975d164763ac1f40c9549db9d5125aaf
Jiten <jpalaparthi@gmail.com>
2025-04-14 15:03:14 +0530

    add more examples and cmake build script

--- 2-demo/README.md
+++ 2-demo/README.md
@@ -4,9 +4,3 @@
 g++ -std=c++17 -I/opt/homebrew/include -L/opt/homebrew/lib -lgtest -lgtest_main -pthread test/test_math_operations.cpp src/math_operations.cpp -o test_runner
 ```
 
-.so
-.a 
-.o
-.lib
-.dll
-.LibDll
---
+++ 2-demo/build/CMakeFiles/test_runner.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_runner.dir/src/math_operations.cpp.o"
+  "CMakeFiles/test_runner.dir/src/math_operations.cpp.o.d"
+  "CMakeFiles/test_runner.dir/test/test_math_operations.cpp.o"
+  "CMakeFiles/test_runner.dir/test/test_math_operations.cpp.o.d"
+  "test_runner"
+  "test_runner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
